fd55b903bde40e73e0081e191a52acbebd22e8d5
liuqiang <qliu@tplhk.cntaiping.com>
2021-11-04 11:20:48 +0800

    20211104

--- PAT1022.cpp
+++ PAT1022.cpp
@@ -5,6 +5,27 @@
 using namespace std;
 typedef long long ll;
 
+
+string change(ll sum,int d){
+    string res="";
+    while(sum){
+        int temp=sum%d;
+        res+=to_string(temp);
+        sum/=d;
+    }
+    reverse(res.begin(),res.end());
+    return res;
+}
 int main(){
+    ll a,b,d,sum=0;
+    cin>>a>>b>>d;
+    if(a==0 && b==0){
+        cout<<0;
+        return 0;
+    }
+    sum=a+b;
+    string res;
+    res= change(sum,d);
+    cout<<res;
     return 0;
 }
