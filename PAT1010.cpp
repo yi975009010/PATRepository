9aa013771b61018c7267488bf6a1d4d9e5030ed6
liuqiang <qliu@tplhk.cntaiping.com>
2022-01-04 15:58:08 +0800

    20220104

--- PAT1010.cpp
+++ PAT1010.cpp
@@ -1,4 +1,22 @@
 //
 // Created by lenovo on 2021/9/7.
 //
+#include<bits/stdc++.h>
+using namespace std;
 
+int main(){
+    int a,b;
+    int flag = 0;
+
+    while(true){
+        scanf("%d%d",&a,&b);
+        if(b){
+            if(flag) printf(" ");
+            else flag = 1;
+
+            printf("%d %d",a*b,b-1);
+        }
+        if(getchar()!=' ') break;
+    }
+    if(flag==0) printf("0 0");
+}
