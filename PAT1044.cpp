b3988ed0aecc89920895012dd9e7127813c78579
liuqiang <qliu@tplhk.cntaiping.com>
2022-01-10 17:25:33 +0800

    20220110

--- PAT1044.cpp
+++ PAT1044.cpp
@@ -4,21 +4,33 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+string earth[13]={"0","jan", "feb", "mar", "apr", "may", "jun", "jly", "aug", "sep", "oct", "nov", "dec"};
+string mark[13]={"tret","tam", "hel", "maa", "huh", "tou", "kes", "hei", "elo", "syy", "lok", "mer", "jou"};
+int a[13] = {0,1,2,3,4,5,6,7,8,9,10,11,12};
 int main(){
-    map<string,int> earth={
-            {"jan",1},{"feb",2},{"mar",3},{"apr",4},{"may",5},{"jun",6},{"jly",7},{"aug",8},{"sep",9},
-            {"oct",10},{"nov",11},{"dec",12}
-    };
-    map<string,int> mark={
-            {"tam",1},{"hel",2},{"maa",3},{"huh",4},{"tou",5},{"kes",6},{"hei",7},{"elo",8},{"syy",9},
-            {"lok",10},{"mer",11},{"jou",12}
-    };
-    /*tam, hel, maa, huh, tou, kes, hei, elo, syy, lok, mer, jou*/
+
+
     int n;
     cin>>n;
     string s;
     for(int i=0;i<n;i++){
-//        cin.getline(a,105);
+        getline(cin,s);
+        string res="";
+        if(s[0]>='0'  && s[0]<='9'){   //是数字的情况
+            int temp= atoi(s.c_str());
+            int a=temp/13;
+            int b=temp%13;
+            cout<<mark[a]<<" "<<earth[b];
+        }
+        else{
+            string temp="";
+            for(int j=0;j<s.size();j++){
+                temp+=s[i];
+                if(s[i]== '){
+
+                }
+            }
+        }
 
     }
 }
--- main.cpp
+++ main.cpp
@@ -1,35 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/*输出a图形*/
-int main() {
-//    std::cout << "Hello, World!" << std::endl;
-
-    int row,col;   //行，列;
-    char c;    //输出图形的字符
-    cin>>col>>c;   //输入行数和列数
-    if(col%2==0)  row=col/2;
-    else row=col/2+1;
-
-    //输出第一行
-    for(int i=0;i<col;i++){
-        cout<<c;
+int n;
+int main(){
+    scanf("%d",&n);
+    double sum=0.0,res=0.0;
+    for(int i=1;i<=n;i++){
+        double temp;
+        scanf("%lf",&temp);
+        sum+=temp*i*(n+1-i);
     }
-    cout<<endl;
-
-    //输出第2-n-1行
-    for(int i=2;i<row;i++){
-        cout<<c;
-        for(int j=0;j<col-2;j++){
-            cout<<' ';
-        }
-        cout<<c<<endl;
-    }
-
-    //输出最后一行
-    for(int i=0;i<col;i++){
-        cout<<c;
-    }
-    cout<<endl;
+    printf("%.2lf",sum);
     return 0;
 }
