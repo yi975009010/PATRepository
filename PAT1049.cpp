5d8036a1cd0c5ba794d648ccf4ab522c36ccb986
liuqiang <qliu@tplhk.cntaiping.com>
2022-01-10 17:31:07 +0800

    20220110

---
+++ PAT1049.cpp
@@ -0,0 +1,21 @@
+//
+// Created by lenovo on 2022/1/10.
+//
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int n;
+int main(){
+    scanf("%d",&n);
+    double sum=0.0,res=0.0;
+    for(int i=1;i<=1;i++){
+        double temp;
+        scanf("%lf",&temp);
+        sum+=temp;
+        res+=sum;
+    }
+    printf("%lf",res);
+    return 0;
+}
+
